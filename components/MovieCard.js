b3bee1917943bd150e671be7dd5977d5fb09003c
RomanMayer7 <romanmayerzon@gmail.com>
2024-02-24 03:22:06 +0200

    Add Navigation & Search Movies Screen

---
+++ components/MovieCard.js
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { View, Modal, Text, Image, StyleSheet, TouchableOpacity } from "react-native"
+
+import MovieInfoModal from "./MovieInfoModal"
+
+export default function MovieCard({ movie, height, width }) {
+    const imageSrc = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+    const [visible, setVisible] = useState(false)
+
+    const openModal = () => setVisible(true)
+    const closeModal = () => setVisible(false)
+
+    return (
+        <>
+            <Modal
+                visible={visible}
+                transparent={true}
+                onRequestClose={closeModal}
+                animationType="slide"
+            >
+                <TouchableOpacity onPress={closeModal} style={{ height: "100%" }} />
+                <MovieInfoModal movie={movie} />
+            </Modal>
+
+            <TouchableOpacity onPress={openModal}>
+                <View style={styles.movieCard}>
+                    {movie.backdrop_path
+                        ? <Image
+                            style={{ height, width, ...styles.image }}
+                            source={{ uri: imageSrc }}
+                        />
+                        : <View style={{ height, width, ...styles.viewAlt }}>
+                            <Text style={styles.textAlt}>{movie.title}</Text>
+                        </View>
+                    }
+                </View>
+            </TouchableOpacity>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    movieCard: {
+        marginVertical: 14,
+    },
+    title: {
+        color: "white"
+    },
+    image: {
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: "#252525",
+    },
+    textAlt: {
+        color: "#FFF",
+        textAlign: "center",
+        fontSize: 25,
+        fontWeight: "400",
+    },
+    viewAlt: {
+        padding: 2,
+        borderColor: "#FFF",
+        borderWidth: 4,
+        borderRadius: 10,
+        display: "flex",
+        justifyContent: "center",
+    }
+})
+
